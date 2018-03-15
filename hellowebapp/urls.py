fc354dbee10fb532143ce904d33ed1e1b05db29c
Hisham <routinodesigns@outlook.com>
2018-03-15 15:39:39 +0100

    Set Up HyperLinks And Individual Pages for items

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -5,13 +5,13 @@ from collection import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    # The new URL entries we're adding:
-    url(r'^about/$',
+    url(r'^about/$', 
         TemplateView.as_view(template_name='about.html'),
         name='about'),
-    url(r'^contact/$',
+    url(r'^contact/$', 
         TemplateView.as_view(template_name='contact.html'),
         name='contact'),
-
+    url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, 
+        name='thing_detail'),
     url(r'^admin/', admin.site.urls),
 ]
