a05ad289478c04cdc578471869f77a1f0f7e98c6
Hisham <routinodesigns@outlook.com>
2018-03-17 18:48:24 +0100

    Password Reset,reset email functionality added.

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.views import (
+   password_reset, 
+   password_reset_done,
+   password_reset_confirm,
+   password_reset_complete
+)
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -24,5 +30,29 @@ urlpatterns = [
 	url(r'^accounts/', 
         include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
-
+	
+    # the new password reset URLs
+    url(r'^accounts/password/reset/$', 
+        password_reset,
+        {'template_name':
+        'registration/password_reset_form.html'},
+        name="password_reset"),
+    url(r'^accounts/password/reset/done/$',
+        password_reset_done,
+        {'template_name':
+        'registration/password_reset_done.html'},
+        name="password_reset_done"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+        password_reset_confirm,
+        {'template_name':
+        'registration/password_reset_confirm.html'},
+        name="password_reset_confirm"),
+    url(r'^accounts/password/done/$', 
+        password_reset_complete,
+        {'template_name':
+        'registration/password_reset_complete.html'},
+        name="password_reset_complete"),
+    url(r'^accounts/',
+        include('registration.backends.simple.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
